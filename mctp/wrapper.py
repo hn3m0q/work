61886fc154a9d202712af1372d8b67f9b6c49280
hn3m0q <hn3m0q@gmail.com>
2022-09-16 11:40:31 -0700

    u

--- mctp/wrapper.py
+++ mctp/wrapper.py
@@ -8,17 +8,36 @@ class MCTPWrapper():
 
         # TODO: enum in python?
         self.msg_type = {'MCTP': 0, 'PLDM':1, 'NCSI':2, 'ETH':3, 'NVME':4, 'SPDM':5, 'SecMsg':6}
-        pass
 
+        self.ncsi_commands = {'clear initial state':{'iid':0, 'command':0},
+                              'select package':{'iid':2, 'command':1},
+                              'deselect package':{'iid':3, 'command':2},
+                              'enable channel':{'iid':4, 'command':3},
+                              'disable channel':{'iid':5, 'command':4},
+                              'reset channel':{'iid':6, 'command':5},
+                              'enable channel network tx': {'iid':7, 'command':6},
+                              'disable channel network rx': {'iid':8, 'command':7},
+                              'set link': {'iid':9, 'command':9}, #008} TODO channel_id, paylen[12] paylen[11]
+                              'get link status': {'iid':0xa, 'command':0xa},
+                              'set vlan filter': {'iid':0xb, 'command':0xb}
+        }
+
+    # TODO check
     def dec_to_2_hex_str(self, dec):
-        if dec > 4095:
+        if dec > 4095: #TODO actually looks like 13 bits?
             raise ValueError("Payload length has 12 bits, can't be more than 4095.")
 
         # ([12:8] and [7:0])
         return ("0", str(hex(dec))) if dec < 255 else (str(hex(dec-255)), str(hex(255)))
 
     def run(self, verbose=True, bus=1, dst_eid=0, msg_type='NCSI', decode_response=True,
-            slave_addr=55, mc_id=0, hrd_rv=1, iid=1, channel_id=0, pay_len=0):
+            slave_addr=55, mc_id=0, hrd_rv=1, iid=1, command=0, channel_id=0, pay_len=0, ncsi_cmdstring=None):
+        # override variables if cmd_string is defined
+        if ncsi_cmdstring:
+            for k in self.ncsi_comands[ncsi_cmdstring]:
+                if self.ncsi_comands[ncsi_cmdstring][k]:
+                    vars()[k] = self.ncsi_comands[ncsi_cmdstring][k]
+
         parsed_pay_len = self.dec_to_2_hex_str(pay_len)
 
         cmd = ["mctp-utils"]
@@ -42,6 +61,7 @@ class MCTPWrapper():
         packet_header.append(str(hrd_rv)),          # HDR_RV
         packet_header.append("0")                   # RSVD
         packet_header.append(str(iid))              # IID
+        packet_header.append(str(command))          # CMD
         packet_header.append(str(channel_id))       # CHANNEL_ID
         packet_header.extend(list(parsed_pay_len))  # PAYLOAD_LEN[12:8], PAYLOAD_LEN[7:0]
         packet_header.extend(["0"] * 8)             # RSVD[63:0]
@@ -68,11 +88,21 @@ class MCTPWrapper():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--verbose', help='Verbose', required=False)
-    parser.add_argument('--slaveaddr', help='Slave address', required=False)
-    parser.add_argument('--mcid', help='MC ID', required=False)
-    parser.add_argument('--headerrv', help='Header revision', required=False)
-    parser.add_argument('--iid', help='IID', required=False)
+    #@note default vals should be the same as default vals in run() so that
+    # both command line and python api behave the same
+    parser.add_argument('-v', '--verbose', help='Verbose', type=bool, default=False, required=False)
+    parser.add_argument('--bus', help='', type=int, default=1, required=False)
+    parser.add_argument('--dst_eid', help='', type=int, default=0, required=False)
+    parser.add_argument('--msg_type', help='', type=str, default='NCSI', required=False)
+    parser.add_argument('--decode_response', help='', type=bool, default=True, required=False)
+    parser.add_argument('--slave_addr', help='Slave address', type=int, default=55, required=False)
+    parser.add_argument('--mc_id', help='MC ID', type=int, default=0, required=False)
+    parser.add_argument('--hrd_rv', help='Header revision', type=int, default=1, required=False)
+    parser.add_argument('--iid', help='IID', type=int, default=1, required=False)
+    parser.add_argument('--command', help='', type=int, default=0, required=False)
+    parser.add_argument('--channel_id', help='', type=int, default=0, required=False)
+    parser.add_argument('--pay_len', help='', type=int, default=0, required=False)
+    parser.add_argument('--ncsi_cmdstring', help='A command string that fill values automatically', type=str, required=False)
     args = vars(parser.parse_args())
     m = MCTPWrapper()
-    m.run()
+    m.run(**args)
