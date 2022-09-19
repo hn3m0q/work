9a2470ac258f11d495076cacaa46e6353e8f0c75
hn3m0q <hn3m0q@gmail.com>
2022-09-19 12:10:59 -0700

    u

--- mctp/wrapper.py
+++ mctp/wrapper.py
@@ -1,8 +1,7 @@
 
-from curses import raw
+import sys
 import subprocess
 import argparse
-from urllib import response
 
 class MCTPWrapper():
     def __init__(self):
@@ -27,7 +26,9 @@ class MCTPWrapper():
         self.fixed_val_keys = ['SRC_ID', 'TAG', 'MSG_TYPE', 'MC_ID', 'HeaderRev', 'Rsv', 'IID',
                                'PacketType', 'Channel']
 
-        self.response = None
+        self.response = dict()
+        self.raw_response = None
+        self.raw_response_list = None
         """
         self.response_parse_list = ["MCTP transport header",
                                     "MCTP header",
@@ -53,18 +54,18 @@ class MCTPWrapper():
         # ([12:8] and [7:0])
         return ("0", str(hex(dec))) if dec < 255 else (str(hex(dec-255)), str(hex(255)))
 
-    def run(self, verbose=True, bus=1, dst_eid=0, msg_type='NCSI', cml_decode_response=True,
+    def run(self, verbose=True, bus=3, dst_eid=0, msg_type='NCSI', cml_decode_response=True,
             slave_addr=55, mc_id=0, hrd_rv=1, iid=1, command=0, channel_id=0, pay_len=0,
             ncsi_cmdstring=None):
         # override variables if cmd_string is defined
         if ncsi_cmdstring:
-            for k in self.ncsi_comands[ncsi_cmdstring]:
-                if self.ncsi_comands[ncsi_cmdstring][k]:
-                    vars()[k] = self.ncsi_comands[ncsi_cmdstring][k]
+            for k in self.ncsi_commands[ncsi_cmdstring]:
+                if self.ncsi_commands[ncsi_cmdstring][k]:
+                    vars()[k] = self.ncsi_commands[ncsi_cmdstring][k]
 
         parsed_pay_len = self.dec_to_2_hex_str(pay_len)
 
-        cmd = ["mctp-utils"]
+        cmd = ["mctp-util"]
 
         # decode response
         if cml_decode_response:
@@ -105,13 +106,15 @@ class MCTPWrapper():
             #print("-------------")
 
             # get raw reponse line
-            response_lines = res.stdout.strip('\n')
+            response_lines = res.stdout.splitlines()
             for i, l in enumerate(response_lines):
                 if "raw response" in l:
                     self.raw_response = response_lines[i+1]
                     break
+            if not self.raw_response:
+                sys.exit("Command failed to have a raw reponse in stdout")
 
-            self.raw_reponse_list = self.raw_response.split(' ')
+            self.raw_response_list = self.raw_response.split(' ')
 
             # assign MC_ID, HDR_RV, .... PAYLOAD_LEN, single byte vals
             for i, val in enumerate(self.raw_response_list):
@@ -120,10 +123,10 @@ class MCTPWrapper():
                 else:
                     break
 
-            self.response['PayLen'] = self.raw_reponse_list[i:i+10]
-            self.response['ResponseCode'] = self.raw_reponse_list[i+10:i+12]
-            self.response['ResponseReason'] = self.raw_reponse_list[i+12:i+14]
-            self.response['Payload'] = self.raw_reponse_list[i+14:]
+            self.response['PayLen'] = self.raw_response_list[i:i+10]
+            self.response['ResponseCode'] = self.raw_response_list[i+10:i+12]
+            self.response['ResponseReason'] = self.raw_response_list[i+12:i+14]
+            self.response['Payload'] = self.raw_response_list[i+14:]
 
             # TODO parse for each example
             if ncsi_cmdstring == 'temperature --':
@@ -132,7 +135,7 @@ class MCTPWrapper():
 
             if verbose:
                 print("Response:")
-                self.pretty(self.reponse)
+                self.pretty(self.response)
 
         except subprocess.CalledProcessError as e:
             print(e.output)
@@ -145,7 +148,7 @@ if __name__ == "__main__":
     # @note default vals should be the same as default vals in run() so that
     #       both command line and python api behave the same
     parser.add_argument('-v', '--verbose', help='Verbose', type=bool, default=False, required=False)
-    parser.add_argument('--bus', help='', type=int, default=1, required=False)
+    parser.add_argument('--bus', help='', type=int, default=3, required=False)
     parser.add_argument('--dst_eid', help='', type=int, default=0, required=False)
     parser.add_argument('--msg_type', help='', type=str, default='NCSI', required=False)
     parser.add_argument('--cml_decode_response', help='', type=bool, default=True, required=False)
